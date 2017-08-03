87d1fc723abdc0fb3259e21d3d719d7c21d60564
Manav Veer Gulati <magulati@paypal.com>
2017-08-03 19:13:27 +0530

    function to get ALL(<=100) restaurant details added
    If the restaurant count is more than 20(Maximum restaurant info returned by the Zomato API) then restaurant count/20 is the number of times we need to invoke the API. That many times the API is invoked and then results are merged and sent to the server.

--- app/controllers/modules/zomato/api/restaurant.js
+++ app/controllers/modules/zomato/api/restaurant.js
@@ -1,48 +1,63 @@
 var express = require('express'),
-  router = express.Router(),
-  keys = require("../../../../../config/keys"),
-  request = require("request"),
-  _ = require("underscore");
+    router = express.Router(),
+    keys = require("../../../../../config/keys"),
+    request = require("request"),
+    _ = require("underscore");
 
-const fetchRestaurant = (lat, lon) => {
-  return new Promise((resolve, reject) => {
-    if(!_.isNumber(lat) || !_.isNumber(lon)) {
-      reject("fetchRestaurant: Invalid Data");
-    }
+const fetchRestaurant = (lat, lon, start) => {
+    return new Promise((resolve, reject) => {
+        if (!_.isNumber(lat) || !_.isNumber(lon)) {
+            reject("fetchRestaurant: Invalid Data");
+        }
 
-    var options = {
-      method: 'GET',
-      url: 'https://developers.zomato.com/api/v2.1/search',
-      qs: {
+        var options = {
+            method: 'GET',
+            url: 'https://developers.zomato.com/api/v2.1/search',
+            qs: {
+                'lat': lat,
+                'lon': lon,
+                'radius': 400,
+                'start': start
+            },
+            headers: {
+                'accept': 'application/json',
+                'user-key': keys.zomato,
+            }
+        };
 
-        'lat': lat,
-        'lon': lon,
-        'radius': 'poiuy'
-      },
-      headers:{
-        'accept': 'application/json',
-        'user-key': keys.zomato,
-      }
-    };
-
-    request(options, (error, response, body) => {
-      if (error)
-        throw reject(error);
-      resolve(JSON.parse(body), response);
+        request(options, (error, response, body) => {
+            if (error)
+                throw reject(error);
+            resolve(JSON.parse(body), response);
+        });
     });
-  });
 };
 
-module.exports = function (app) {
-  app.use('/', router);
-
-  router.get('/fetch', (req, res) => {
-    const lat = 12.8471445, lon=80.2265543;
-    fetchRestaurant(lat, lon).then((body, response) => {
-      res.send(body);
-    }).catch((err) => {
-      console.log(err);
-      res.send(err);
-    })
-  });
+module.exports = function(app) {
+    app.use('/', router);
+    router.get('/fetch', (req, res) => {
+        let body = "hello";
+        var no_of_results;
+        var start = 0;
+        var arr = [];
+        const lat = 12.8471445,
+            lon = 80.2265543;
+        fetchRestaurant(lat, lon, start).then((body, response) => {
+            no_of_results = body.results_found;
+            if (no_of_results > 20) {
+                array_size = Math.floor(no_of_results / 20);
+                console.log(array_size);
+                for (var i = 0; i < array_size; i++) {
+                    arr.push(fetchRestaurant(lat, lon, start));
+                    start += 20;
+                }
+            }
+            Promise.all(arr).then((results) => {
+                res.send(results);
+            });
+        }).catch((err) => {
+            console.log(err);
+            //sres.send(err);
+        })
+    });
 };
